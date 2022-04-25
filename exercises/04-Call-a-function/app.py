999e653effc04170caa117ea692f3496240f0cea
Dans182 <dgaiteiro@gmail.com>
2022-04-25 12:59:00 +0000

    4

--- exercises/04-Call-a-function/app.py
+++ exercises/04-Call-a-function/app.py
@@ -2,3 +2,10 @@ def calculate_area(length,edge):
 	return length * edge
 
 # Your code Below this line:
+square_area1 = calculate_area(4,4)
+square_area2 = calculate_area(2,2)
+square_area3 = calculate_area(5,5)
+
+print(square_area1)
+print(square_area2)
+print(square_area3)
